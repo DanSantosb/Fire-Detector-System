b1296e27bc49884dbc5c762265eeb0f80bc1f6f8
Danilo Santos <danilosantos.contato27@gmail.com>
2025-07-15 20:42:24 -0300

    Update main.c

--- Projeto_final/src/main.c
+++ Projeto_final/src/main.c
@@ -16,20 +16,22 @@ void disable_watchdog(void) {
 int main(void) {
   disable_watchdog();
   gpio_setup();
-  
+  timer7_setup();
+
   GPIO1_SETDATAOUT = LED_WHITE;
   uart0_write_str("CALIBRANDO SENSORES, ESPERE UM POUCO...\r\n");
-  _delay_sec(300);
+  _delay_sec(10);
   GPIO1_CLEARDATAOUT = LED_WHITE;
   uart0_write_str("SENSORS CALIBRADOS :)\r\n");
     
     //Loop principal
   while (1){
-    if (MQ2_VALUE | MQ6_VALUE == 1){
+    char entrada = uart0_getc();
+    if (entrada - '0' == 1){
       changeGreen(); 
-    } else if (MQ2_VALUE| MQ6_VALUE == 2){
+    } else if (entrada - '0' == 2){
       changeYellow();
-    } else if (MQ2_VALUE| MQ6_VALUE == 3){
+    } else if (entrada - '0' == 3){
       changeRed();
     }
   }
